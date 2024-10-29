e8768b84ed63b1b842e0e2c80e9c00991f49188d
Damien Tan <74666853+DamienTan01@users.noreply.github.com>
2024-10-29 10:15:03 +0800

    Integrate on Mac

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// import type { NextConfig } from "next";
+import('next').NextConfig;
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+const config = {
+  reactStrictMode: true,
+};
+
+module.exports = config;
+
